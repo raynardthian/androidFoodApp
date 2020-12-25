8d932370495509ef7805fb9289f804c041cd48ad
Raynard <raynard2018@gmail.com>
2020-12-26 00:26:41 +0800

    Check for duplicates for registration

--- app/src/main/java/ece/np/edu/miniproject2_raynardthian/RegisterActivity.java
+++ app/src/main/java/ece/np/edu/miniproject2_raynardthian/RegisterActivity.java
@@ -1,5 +1,6 @@
 package ece.np.edu.miniproject2_raynardthian;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,28 +14,41 @@ public class RegisterActivity extends AppCompatActivity {
     EditText etUsername, etPassword, etAge, etAddress;
     Button btRegister; //btViewAll;
     ListView lvNames;
-
+    DataBaseHelper db;
     private View.OnClickListener registerListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Customer customerList;
-//            String name = etUsername.getText().toString();
+            String name = etUsername.getText().toString();
 //            String pass = etPassword.getText().toString();
 //            Integer age = Integer.parseInt(etAge.getText().toString());
 //            String address = etAddress.getText().toString();
-            try {
-                customerList = new Customer(-1, etUsername.getText().toString(), etPassword.getText().toString(), Integer.parseInt(etAge.getText().toString()), etAddress.getText().toString()); // Address not passing through
-                Toast.makeText(RegisterActivity.this, customerList.toString(), Toast.LENGTH_SHORT).show();
-
-            } catch (Exception e) {
-                Toast.makeText(RegisterActivity.this, "Error Creating Customer", Toast.LENGTH_SHORT).show();
-                customerList = new Customer(-1, "Error", "Error Password", 0, "Error Address");
+            Cursor cursor = db.getAllData();
+            cursor.moveToFirst();
+            boolean exist = false;
+            for (int i = 0; i < cursor.getCount(); i++) {
+                String nameData = cursor.getString(1);
+                if (name.equals(nameData)) {
+                    exist=true;
+                } else {
+                    cursor.moveToNext();
+                }
             }
-            DataBaseHelper dataBaseHelper = new DataBaseHelper(RegisterActivity.this);
-            boolean success = dataBaseHelper.addOne(customerList);
-            Toast.makeText(RegisterActivity.this, "Created Successfully", Toast.LENGTH_SHORT).show();
-
+            if(exist){
+                Toast.makeText(RegisterActivity.this, "Username is taken", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                try {
+                    customerList = new Customer(-1, etUsername.getText().toString(), etPassword.getText().toString(), Integer.parseInt(etAge.getText().toString()), etAddress.getText().toString()); // Address not passing through
+                    Toast.makeText(RegisterActivity.this, customerList.toString(), Toast.LENGTH_SHORT).show();
+                    db.addOne(customerList);
+                    Toast.makeText(RegisterActivity.this, "Created Successfully", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(RegisterActivity.this, "Error Creating Customer", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
+
     };
 //    private View.OnClickListener viewListener = new View.OnClickListener() {
 //        @Override
@@ -57,6 +71,7 @@ public class RegisterActivity extends AppCompatActivity {
         btRegister = this.findViewById(R.id.btRegister);
         etAddress = this.findViewById(R.id.etAddress);
         lvNames = this.findViewById(R.id.lvNames);
+        db = new DataBaseHelper(RegisterActivity.this);
 //        btViewAll = this.findViewById(R.id.btViewAll);
 
 //        btViewAll.setOnClickListener(viewListener);

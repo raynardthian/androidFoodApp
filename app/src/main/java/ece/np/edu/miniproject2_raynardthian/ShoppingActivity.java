b79448f9b3c0dc9d7cbef20baa5d3df8ce918b1c
Raynard <raynard2018@gmail.com>
2020-12-29 01:41:20 +0800

    Integrating RecyclerView

--- app/src/main/java/ece/np/edu/miniproject2_raynardthian/ShoppingActivity.java
+++ app/src/main/java/ece/np/edu/miniproject2_raynardthian/ShoppingActivity.java
@@ -7,15 +7,28 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ShoppingActivity extends AppCompatActivity {
-    Button btProfile;
-    ImageView ivChicken,ivMac,ivCutlet;
+    Button btProfile, btGo;
+    ImageView ivChicken, ivMac, ivCutlet;
+    RecyclerView rvShop;
+    RecyclerView.Adapter adapter;
+    ArrayList<String> users;
     private View.OnClickListener profileListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent i = new Intent(ShoppingActivity.this,ProfileActivity.class);
+            Intent i = new Intent(ShoppingActivity.this, ProfileActivity.class);
             startActivity(i);
+        }
+    };
+    private View.OnClickListener chickenListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
         }
     };
 
@@ -23,11 +36,27 @@ public class ShoppingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping);
-        btProfile= this.findViewById(R.id.btProfile);
+        btProfile = this.findViewById(R.id.btProfile);
         ivChicken = this.findViewById(R.id.ivChicken);
         ivCutlet = this.findViewById(R.id.ivCutlet);
         ivMac = this.findViewById(R.id.ivMac);
+        btGo = this.findViewById(R.id.btGo);
+        rvShop = this.findViewById(R.id.rvShop);
 
+        users = new ArrayList<>();
+        users.add("Chicken Rice");
+        users.add("Chicken Cutlet");
+        users.add("Mac and Cheese");
+//        for (int i = 0; i < 10; i++) {
+//            users.add("hello " + i);
+//            System.out.println(i);
+//        }
+
+        rvShop.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdapter(users);
+        rvShop.setAdapter(adapter);
+
+        ivChicken.setOnClickListener(chickenListener);
         btProfile.setOnClickListener(profileListener);
     }
 }
